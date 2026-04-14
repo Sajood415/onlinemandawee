9bd696fd2f3c8f760cb480ce74324b4c3ecc5e34
razaaliwebdev <razaali.webdev@gmail.com>
2026-04-14 22:16:23 +0500

    feat: add missing file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  jsxImportSource: 'react',
 };
 
 const withNextIntl = createNextIntlPlugin();
